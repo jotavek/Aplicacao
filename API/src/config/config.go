bbdc6ca78df3a28949e4602f78f8fcfc14193713
João Victor Galvão de Oliveira <jotavedev@gmail.com>
2024-05-17 18:36:01 -0300

    config

---
+++ API/src/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	// StringConexaoBanco é a string de conexão com o MySQL
+	StringConexaoBanco = ""
+
+	// Porta onde a API vai estar rodando
+	Porta = 0
+
+	// SecretKey é a chave que vai ser usada para assinar o token
+	SecretKey []byte
+)
+
+// Carregar vai inicializar as variáveis de ambiente
+func Carregar() {
+	var erro error
+
+	if erro = godotenv.Load(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	// Os dados do arquivo .env por padrão são reconhecidos em string, entao essa função strconv converte para inteiro, é como se fosse o ParseInt, se der erro a porta é redirecionada para porta 9000
+	Porta, erro = strconv.Atoi(os.Getenv("API_PORT"))
+	if erro != nil {
+		Porta = 9000
+	}
+
+	StringConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USUARIO"),
+		os.Getenv("DB_SENHA"),
+		os.Getenv("DB_NOME"),
+	)
+
+	SecretKey = []byte(os.Getenv("SECRET_KEY"))
+}
